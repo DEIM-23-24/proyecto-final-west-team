bb3d463d389564697431106202d176e0963f09b2
miki <miguel.perez8@ciclosformativosceu.es>
2024-02-08 18:29:49 +0100

    Actu Menu

---
+++ Assets/Modelos/Imagenes menu/moverPistola.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moverPistola : MonoBehaviour
+{
+public float posStart;
+public float posExit;
+public bool arriba;
+    // Start is called before the first frame update
+    void Start()
+    {
+        arriba = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Vertical") < 0)
+        arriba = false;
+        if (Input.GetAxis("Vertical") > 0)
+        arriba  = true;
+        if(arriba){
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(-806, posStart, 0);
+        }else{
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(-806, posExit, 0);
+            }
+    }
+}
